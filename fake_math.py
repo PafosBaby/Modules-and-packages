0d6767b6b2f678a1c6f7692a5ef5d95b07c519c5
aleksey <i@pafosbaby.ru>
2024-11-28 18:25:37 +0700

    Домашняя работа по уроку "Модули и пакеты"

---
+++ fake_math.py
@@ -0,0 +1,8 @@
+
+
+def divide(first , second):
+    if second == 0:
+         return 'На ноль делить нельзя!'
+    else:
+         return first / second
+
---
+++ module.py
@@ -0,0 +1,8 @@
+import fake_math as fm
+import true_math as tm
+
+print(fm.divide(10,0))
+print(tm.divide(10,0))
+print(fm.divide(10,5))
+print(tm.divide(10,2))
+
